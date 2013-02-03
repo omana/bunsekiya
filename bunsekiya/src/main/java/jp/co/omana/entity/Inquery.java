2e1d0a6173831dcc352e277b2fcef2e16d5a3597
Hiroki <pero2.info@gmail.com>
2013-02-03 18:37:24 +0900

    テスト

--- bunsekiya/src/main/java/jp/co/omana/entity/Inquery.java
+++ bunsekiya/src/main/java/jp/co/omana/entity/Inquery.java
@@ -17,6 +17,7 @@ public class Inquery implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    
     /** inqueryIdプロパティ */
     @Id
     @Column(precision = 10, nullable = false, unique = true)
